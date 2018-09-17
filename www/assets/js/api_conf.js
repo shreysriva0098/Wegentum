321e84e5c326b706e66676f04d0ce2d37615dd01
Shrey Prakash <shreysriva@gmail.com>
2018-09-18 01:50:24 +0530

    Initial

--- www/assets/js/api_conf.js
+++ www/assets/js/api_conf.js
@@ -5,14 +5,14 @@
  */
 
 
-var apiUrl = "https://wegentum.com/wesight/index.php/";
+var apiUrl = "http://45.77.9.75/Client/index.php/";
 //var apiUrl = "app/index.php/";
 
-var uploadpath = "https://wegentum.com/wesight/upload/";
+var uploadpath = "http://45.77.9.75/Client/upload/";
 //var uploadpath="app/upload";
 
-var document_path = "https://wegentum.com/crm/";
+var document_path = "http://45.77.9.75/wegentum_app/";
 //var document_path="app/upload/documents/";
 
-var baseUrl = "https://wegentum.com/wesight/";
+var baseUrl = "http://45.77.9.75/Client/";
 //var uploadpath="app/upload";
